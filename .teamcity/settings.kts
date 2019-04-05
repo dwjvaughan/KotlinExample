03f87819ff8e3937d7775700c23671cbaae22bcd
Dave Vaughan <david.vaughan@devopsgroup.com>
2019-04-05 17:19:47 +0100

    wut

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,7 +31,7 @@ project {
     name = "Hello world"
     steps {
         script {
-            scriptContent = "liquibase --password=${env.LIQUIBASE_PASSWORD} --defaultsFile=cf-mysql-01.properties migrate"
+            scriptContent = "liquibase --password=${env.LIQUIBASE_PASSWORD} --defaultsFile=cf-mysql-01.properties migrate "
         }
     }
   }
